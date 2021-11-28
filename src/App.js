b768729c3358c0319bb2063678cb715d369076aa
Oleh <okopylov95@gmail.com>
2021-11-29 01:11:22 +0300

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import Nice from "./assets/Jordan-1.jpg";
+import Adidas from "./assets/adidas.jpg";
+import Puma from "./assets/puma.jpg.";
+import NewBalance from "./assets/new-balance.jpg.";
+
 import './App.css';
 
-function App() {
+const MyHeader = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+        <h1>Sneakers</h1>
+            <h2>Products List View</h2>
+            <h2>Products View</h2>
+            <h2>My Favorites</h2>
     </div>
   );
 }
 
+const Items = () => {
+  return (
+    <div className="product_list">
+        <h1> nike air jordan 1</h1>
+            <img src={Nice}/>
+        <h1> adidas Yeezy Boost</h1>
+            <img src={Adidas}/>
+        <h1> puma clyde</h1>
+            <img src={Puma}/>
+        <h1> new-balance</h1>
+            <img src={NewBalance}/>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+  <div className="App">
+    <MyHeader />
+    <Items />
+    </div>
+  )
+} 
 export default App;
